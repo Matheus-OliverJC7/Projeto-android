e0967631c11e4c621f5b8a38e92978d3d0923f37
matheusoliveirajc <eumatheusdeoliveirajc@gmail.com>
2024-03-21 11:30:18 -0300

    finalized

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 const lines = document.querySelectorAll('.line');
 const menuIcon = document.querySelector('.menu-mobile-icon');
-menuIcon.addEventListener('click', function(){
+const menu = document.querySelector('.menu-mobile');
+menuIcon.addEventListener('click', activeMenu);
+function activeMenu(){
     let classList = ['active-line1','active-line2','active-line3'];
     for(let i = 0;i<lines.length;i++){
         lines[i].classList.toggle(classList[i]);
     }
-});
+    document.body.classList.toggle('body-overflow');
+    menu.classList.toggle('active-menu');
+}
