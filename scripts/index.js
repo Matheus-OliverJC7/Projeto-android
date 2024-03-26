16e518dcf864fe37885aad289869ab68ece1206b
matheusoliveirajc <eumatheusdeoliveirajc@gmail.com>
2024-03-26 12:55:04 -0300

    f

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,17 @@ menuIcon.addEventListener('click', activeMenu);
 function activeMenu(){
     let classList = ['active-line1','active-line2','active-line3'];
     for(let i = 0;i<lines.length;i++){
-        lines[i].classList.toggle(classList[i]);
+      lines[i].classList.toggle(classList[i]);
     }
-    document.body.classList.toggle('body-overflow');
     menu.classList.toggle('active-menu');
+    document.addEventListener('click', function(event){
+      var isClickMenu = menu.contains(event.target);
+      var isClickIcon = menuIcon.contains(event.target);
+      if(!isClickMenu && !isClickIcon){
+        for(let i = 0;i<lines.length;i++){
+          lines[i].classList.toggle(classList[i]);
+      }
+        menu.classList.remove('active-menu');
+      }
+    });
 }
