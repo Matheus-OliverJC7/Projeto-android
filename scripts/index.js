ef8d9a881133ddcdbfdd8109f68b03c2d989f1f7
matheusoliveirajc <eumatheusdeoliveirajc@gmail.com>
2024-07-02 10:38:36 -0300

    w

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,25 @@ const menuIcon = document.querySelector('.menu-mobile-icon');
 const menu = document.querySelector('.menu-mobile');
 const navItems = document.querySelectorAll('.nav-item');
 menuIcon.addEventListener('click', activeMenu);
+const classList = ['active-line1','active-line2','active-line3'];
 function activeMenu(){
   document.body.classList.toggle('blockScroll-body');
-    let classList = ['active-line1','active-line2','active-line3'];
     for(let l = 0;l<lines.length;l++){
       lines[l].classList.toggle(classList[l]);
     }
     menu.classList.toggle('active-menu');
-    document.addEventListener('click', function(event){
-      let isClickMenu = menu.contains(event.target);
-      let isClickIcon = menuIcon.contains(event.target);
-      for(let i = 0;i<navItems.length;i++){
-        let isClickItem = navItems[i].contains(event.target);
-        if(!isClickMenu && !isClickIcon || isClickItem){
-          document.body.classList.remove('blockScroll-body');
-          for(let l = 0;l<lines.length;l++){
-            lines[l].classList.remove(classList[l]);
-          }
-          menu.classList.remove('active-menu');
-        }
+    }
+document.addEventListener('click', function(event){
+  let isClickMenu = menu.contains(event.target);
+  let isClickIcon = menuIcon.contains(event.target);
+  for(let i = 0;i<navItems.length;i++){
+    let isClickItem = navItems[i].contains(event.target);
+    if(!isClickMenu && !isClickIcon || isClickItem){
+      document.body.classList.remove('blockScroll-body');
+      for(let l = 0;l<lines.length;l++){
+        lines[l].classList.remove(classList[l]);
       }
-    });
-}
+      menu.classList.remove('active-menu');
+    }
+  }
+});
